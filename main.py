5675d9279248c8a9acd6a7ed1a7935fc669ecea3
Afiya57 <aafiyahhussain57@gmail.com>
2024-10-01 10:30:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+
+def options(comp, user):
+  if comp == user:
+    return 0
+  if comp == 0 and user == 1:
+    return -1
+  if comp == 1 and user == 2:
+    return -1
+  if comp == 2 and user == 0:
+    return -1
+  else:
+    return 1
+
+comp =random.randint(1, 3)
+
+user = int(input("1 for snake, 2 for paper and 3 for scissors: "))
+score = options(comp, user)
+
+print("\nYou: ", user)
+print("computer: ", comp)
+
+if score == 0:
+  print("It's a draw")
+elif score == -1:
+  print("You lose")
+else:
+  print("You won")
